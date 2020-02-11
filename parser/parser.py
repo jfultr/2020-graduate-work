2cd8b5890e878ae3b91e2a11dd0a54ecdf1ed962
Yakubov Farkhat <f.yakubov@orion-m2m.com>
2020-02-11 17:04:27 +0600

    Simple threading in parser.py; Added exception; gitignore fixed

--- parser/parser.py
+++ parser/parser.py
@@ -1,8 +1,36 @@
 import requests
+import threading
+import time
 
-url = 'http://127.0.0.1/cam_pic.php'
-p = requests.get(url)
-out = open("img.jpg", "wb")
-out.write(p.content)
-out.close()
 
+url = 'http://169.254.45.12/html/cam_pic.php'
+
+
+def catch_path_exception(write_funck):
+    def decorated_function(path, content):
+        try:
+            write_funck(path, content)
+        except OSError:
+            print('catched!!')
+    return decorated_function
+
+
+@catch_path_exception
+def write_img(file_path, content):
+    with open(file_path, "wb") as out:
+        out.write(content.content)
+        out.close()
+
+
+def main_loop():
+    while True:
+        start = time.time()
+        # time.sleep(0.1)
+        p = requests.get(url)
+        file_path = 'img.jpg'
+        write_img(file_path, p)
+        print(start - time.time())
+
+
+if __name__ == '__main__':
+    thread = threading.Thread(target=main_loop).start()
