21d98bd534d8e37e5d10c855b42762efbb11ec9c
yakubov <farhatyakubov@gmail.com>
2020-03-30 15:27:32 +0600

    requests replaced by opencv

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-import requests
+import cv2
 import threading
-import time
-
-url = 'http://169.254.45.12/html/cam_pic.php'
-file_path = 'tmp/img.jpg'
-
-
-def catch_path_exception(write_funck):
-    def decorated_function(path, content):
-        try:
-            write_funck(path, content)
-        except OSError:
-            print('catched!!')
-
-    return decorated_function
-
-
-@catch_path_exception
-def write_img(file_path, content):
-    with open(file_path, "wb") as out:
-        out.write(content.content)
-        out.close()
 
 
 def main_loop():
+    cap = cv2.VideoCapture('http://169.254.104.205:8080/?action=stream')
+
     while True:
-        start = time.time()
-        p = requests.get(url)
-        write_img(file_path, p)
-        print(start - time.time())
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('gray', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     thread = threading.Thread(target=main_loop).start()
+    thread.start()
