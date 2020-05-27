bdb97c9d49e1dd9d3e0bf5f23eb66b2c194a41e7
yakubov <farhatyakubov@gmail.com>
2020-05-28 05:30:18 +0600

    Simple massage transmit

--- Agent/Raspberry/server.py
+++ Agent/Raspberry/server.py
@@ -1,10 +1,14 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('', 1234))
 s.listen(5)
 
 while True:
     clientsocket, address = s.accept()
     print(f'Connection from {address}')
     clientsocket.send(bytes("Welcome to the server", "utf-8"))
+    msg = clientsocket.recv(1024)
+    print(msg)
+    clientsocket.close()
--- Server/client.py
+++ Server/client.py
@@ -1,7 +1,8 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+s.connect(('169.254.49.227', 1234))
 
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
+s.send(bytes("0,0,0,200,200", "utf-8"))
