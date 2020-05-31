0cfcf2d173248534c3aeec96c46e3db3d74808e0
yakubov <farhatyakubov@gmail.com>
2020-05-31 06:18:00 +0600

    Added markers

--- Server/client.py
+++ Server/client.py
@@ -5,4 +5,6 @@ s.connect(('169.254.49.227', 1234))
 
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
+
+# left back, right back, left front, right front, second left, second right
 s.send(bytes("0,0,0,0,0,0", "utf-8"))
