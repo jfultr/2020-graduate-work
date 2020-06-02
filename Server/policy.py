a9ff0b18d4a8c6b60a4a03e3948eb2e991565006
yakubov <farhatyakubov@gmail.com>
2020-06-03 02:39:09 +0600

    Added raw policy file with Policy Gradients;

---
+++ Server/policy.py
@@ -0,0 +1,123 @@
+import gym
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def render_policy_net(model, n_max_steps=200, seed=42):
+    frames = []
+    env = Environment()
+    env.seed(seed)
+    np.random.seed(seed)
+    obs = env.reset()
+    for step in range(n_max_steps):
+        frames.append(env.render(mode="rgb_array"))
+        left_proba = model.predict(obs.reshape(1, -1))
+        action = int(np.random.rand() > left_proba)
+        obs, reward, done, info = env.step(action)
+        if done:
+            break
+    env.close()
+    return frames
+
+
+def update_scene(num, frames, patch):
+    patch.set_data(frames[num])
+    return patch,
+
+
+def plot_animation(frames, repeat=False, interval=40):
+    fig = plt.figure()
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+    anim = animation.FuncAnimation(
+        fig, update_scene, fargs=(frames, patch),
+        frames=len(frames), repeat=repeat, interval=interval)
+    plt.close()
+    return anim
+
+
+def play_one_step(env, obs, model, loss_fn):
+    with tf.GradientTape() as tape:
+        left_proba = model(obs[np.newaxis])
+        action = (tf.random.uniform([1, 1]) > left_proba)
+        y_target = tf.constant([[1.]]) - tf.cast(action, tf.float32)
+        loss = tf.reduce_mean(loss_fn(y_target, left_proba))
+    grads = tape.gradient(loss, model.trainable_variables)
+    obs, reward, done, info = env.step(int(action[0, 0].numpy()))
+    return obs, reward, done, grads
+
+
+def play_multiple_episodes(env, n_episodes, n_max_steps, model, loss_fn):
+    all_rewards = []
+    all_grads = []
+    for episode in range(n_episodes):
+        current_rewards = []
+        current_grads = []
+        obs = env.reset()
+        for step in range(n_max_steps):
+            obs, reward, done, grads = play_one_step(env, obs, model, loss_fn)
+            current_rewards.append(reward)
+            current_grads.append(grads)
+            if done:
+                break
+        all_rewards.append(current_rewards)
+        all_grads.append(current_grads)
+    return all_rewards, all_grads
+
+
+def discount_rewards(rewards, discount_factor):
+    discounted = np.array(rewards)
+    for step in range(len(rewards) - 2, -1, -1):
+        discounted[step] += discounted[step + 1] * discount_factor
+    return discounted
+
+
+def discount_and_normalize_rewards(all_rewards, discount_factor):
+    all_discounted_rewards = [discount_rewards(rewards, discount_factor)
+                              for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [(discounted_rewards - reward_mean) / reward_std
+            for discounted_rewards in all_discounted_rewards]
+
+
+n_iterations = 150
+n_episodes_per_update = 10
+n_max_steps = 200
+discount_factor = 0.95
+optimizer = keras.optimizers.Adam(lr=0.01)
+loss_fn = keras.losses.binary_crossentropy
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+model = keras.models.Sequential([
+    keras.layers.Dense(5, activation='elu', input_shape=[4]),
+    keras.layers.Dense(1, activation='sigmoid'),
+])
+
+print('start training')
+env = Environment()
+env.seed(42)
+for iteration in range(n_iterations):
+    all_rewards, all_grads = play_multiple_episodes(
+        env, n_episodes_per_update, n_max_steps, model, loss_fn)
+    all_final_rewards = discount_and_normalize_rewards(all_rewards,
+                                                       discount_factor)
+    all_mean_grads = []
+    for var_index in range(len(model.trainable_variables)):
+        mean_grads = tf.reduce_mean(
+            [final_reward * all_grads[episode_index][step][var_index]
+             for episode_index, final_rewards in enumerate(all_final_rewards)
+                for step, final_reward in enumerate(final_rewards)], axis=0)
+        all_mean_grads.append(mean_grads)
+    optimizer.apply_gradients(zip(all_mean_grads, model.trainable_variables))
+
+env.close()
+print('training ends')
+frames = render_policy_net(model)
+plot_animation(frames)
