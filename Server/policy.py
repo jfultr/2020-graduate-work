d8fe57c51df6273526495d05322762a4d6404152
yakubov <farhatyakubov@gmail.com>
2020-06-03 02:43:46 +0600

    Small fake corrections

--- Server/policy.py
+++ Server/policy.py
@@ -1,19 +1,15 @@
-import gym
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from environment import Environment
 
 
 def render_policy_net(model, n_max_steps=200, seed=42):
     frames = []
     env = Environment()
-    env.seed(seed)
     np.random.seed(seed)
     obs = env.reset()
     for step in range(n_max_steps):
-        frames.append(env.render(mode="rgb_array"))
         left_proba = model.predict(obs.reshape(1, -1))
         action = int(np.random.rand() > left_proba)
         obs, reward, done, info = env.step(action)
@@ -26,17 +22,6 @@ def render_policy_net(model, n_max_steps=200, seed=42):
 def update_scene(num, frames, patch):
     patch.set_data(frames[num])
     return patch,
-
-
-def plot_animation(frames, repeat=False, interval=40):
-    fig = plt.figure()
-    patch = plt.imshow(frames[0])
-    plt.axis('off')
-    anim = animation.FuncAnimation(
-        fig, update_scene, fargs=(frames, patch),
-        frames=len(frames), repeat=repeat, interval=interval)
-    plt.close()
-    return anim
 
 
 def play_one_step(env, obs, model, loss_fn):
